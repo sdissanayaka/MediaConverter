ee2331d1ffd2bf7ea197571314d363bea3aa8232
Shanilka Herath <shanilkayasas@gmail.com>
2020-07-19 21:59:49 -0700

    Implementation of main class and HashMap

---
+++ src/MediaConverter/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MediaConverter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Shanilka
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        
+        System.out.println("start Converting Media Files");
+        
+        Map<String, String> mediaFilelink = new HashMap();
+        mediaFilelink.put("string","String");
+        mediaFilelink.put("string","String");
+        
+        System.out.println("Finished Converting Media Files");
+        
+        
+        
+        
+        
+    }
+    
+}
